d440b902fa9fb0a6df52c13424afa16374d9bbf7
liobouchan <leo.bouchan@gmail.com>
2015-02-14 02:06:10 -0600

    Vistas Principal, Login, BIenvenida y algunas alertas agregadas

--- PlataformaDeJuegos/src/plataformadejuegos/PlataformaDeJuegos.java
+++ PlataformaDeJuegos/src/plataformadejuegos/PlataformaDeJuegos.java
@@ -1,22 +1,28 @@
+package plataformadejuegos;
+
+import Vistas.Bienvenida;
+import Vistas.Frame;
+import java.awt.BorderLayout;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-package plataformadejuegos;
-
 /**
  *
  * @author lio
  */
 public class PlataformaDeJuegos {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Frame frame = new Frame();
+        Bienvenida bienvenida = new Bienvenida();
+        frame.setLayout(new BorderLayout());
+        frame.add(bienvenida, BorderLayout.CENTER);
+        frame.setVisible(true);
     }
-    
 }
